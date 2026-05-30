02ff991bb8bb58244c7bd68d20360e858f46b761
EltrutLikes <gesc808@gmail.com>
2026-05-29 23:59:36 -0400

    register myalite colour

--- src/main/java/co/eltrut/lepton/core/Lepton.java
+++ src/main/java/co/eltrut/lepton/core/Lepton.java
@@ -1,8 +1,11 @@
 package co.eltrut.lepton.core;
 
+import co.eltrut.differentiate.client.provider.MyaliteColorProvider;
+import co.eltrut.lepton.core.registry.LeptonBlocks;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -20,5 +23,23 @@ public class Lepton {
 
         REGISTRATOR.register(modEventBus);
 
+        modEventBus.addListener(this::registerBlockColorHandlers);
+        modEventBus.addListener(this::registerItemColorHandlers);
+
     }
+
+    private void registerBlockColorHandlers(RegisterColorHandlersEvent.Block event) {
+        event.register(MyaliteColorProvider.getBlockColor(),
+                LeptonBlocks.POLISHED_MYALITE_BUTTON.value(),
+                LeptonBlocks.POLISHED_MYALITE_PRESSURE_PLATE.value(),
+                LeptonBlocks.POLISHED_MYALITE_WALL.value());
+    }
+
+    private void registerItemColorHandlers(RegisterColorHandlersEvent.Item event) {
+        event.register(MyaliteColorProvider.getItemColor(),
+                LeptonBlocks.POLISHED_MYALITE_BUTTON.value(),
+                LeptonBlocks.POLISHED_MYALITE_PRESSURE_PLATE.value(),
+                LeptonBlocks.POLISHED_MYALITE_WALL.value());
+    }
+
 }
