da817584c6fb0bd7cdbc707d7e153f21c448835d
EltrutLikes <gesc808@gmail.com>
2026-07-12 18:42:52 -0400

    wow is this incredibly frustrating

--- src/main/java/co/eltrut/lepton/core/compat/LeptonEveryCompatModule.java
+++ src/main/java/co/eltrut/lepton/core/compat/LeptonEveryCompatModule.java
@@ -6,12 +6,14 @@ import co.eltrut.lepton.core.Lepton;
 import co.eltrut.lepton.core.registry.LeptonBlocks;
 import net.mehvahdjukaar.every_compat.api.*;
 import net.mehvahdjukaar.moonlight.api.resources.pack.ResourceGenTask;
+import net.mehvahdjukaar.moonlight.api.set.wood.VanillaWoodChildKeys;
 import net.mehvahdjukaar.moonlight.api.set.wood.VanillaWoodTypes;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SlabBlock;
@@ -27,6 +29,11 @@ public class LeptonEveryCompatModule extends SimpleModule {
     public final SimpleEntrySet<WoodType, Block> woodVerticalSlab;
     public final SimpleEntrySet<WoodType, Block> woodWall;
 
+    public final SimpleEntrySet<WoodType, Block> strippedWoodSlab;
+    public final SimpleEntrySet<WoodType, Block> strippedWoodStairs;
+    public final SimpleEntrySet<WoodType, Block> strippedWoodVerticalSlab;
+    public final SimpleEntrySet<WoodType, Block> strippedWoodWall;
+
     public static void registerEveryCompatModule() {
         // goofy ass abstraction
         EveryCompatAPI.registerModule(new LeptonEveryCompatModule());
@@ -37,43 +44,100 @@ public class LeptonEveryCompatModule extends SimpleModule {
 
         woodSlab = SimpleEntrySet.builder(WoodType.class, "wood_slab", LeptonBlocks.OAK.woods().getSlabBlock(),
                 () -> VanillaWoodTypes.OAK, s -> new SlabBlock(Utils.copyPropertySafe(s.log)))
+                .requiresChildren(VanillaWoodChildKeys.WOOD, VanillaWoodChildKeys.LOG)
                 .addTexture(ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.MINECRAFT, "oak_log"), PaletteStrategies.LOG_SIDE_STANDARD)
+                .includeModelsBlock(
+                        ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.MINECRAFT, "oak_wood"),
+                        ResourceLocation.fromNamespaceAndPath(Lepton.MOD_ID, "oak_wood_slab"),
+                        ResourceLocation.fromNamespaceAndPath(Lepton.MOD_ID, "oak_wood_slab_top")
+                )
                 .setTab(() -> BuiltInRegistries.CREATIVE_MODE_TAB.get(CreativeModeTabs.BUILDING_BLOCKS))
                 .setTabMode(TabAddMode.AFTER_SAME_WOOD)
                 .addTag(ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.DIFFERENTIATE, "wood_slabs"), Registries.BLOCK, Registries.ITEM)
+                .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
                 .addRecipe(ResourceLocation.fromNamespaceAndPath(Lepton.MOD_ID, "crafting/wood_slab/oak_wood_slab"))
                 .build();
         this.addEntry(woodSlab);
 
         woodStairs = SimpleEntrySet.builder(WoodType.class, "wood_stairs", LeptonBlocks.OAK.woods().getStairsBlock(),
                 () -> VanillaWoodTypes.OAK, s -> new StairBlock(VanillaWoodTypes.OAK.log.defaultBlockState(), Utils.copyPropertySafe(s.log)))
+                .requiresChildren(VanillaWoodChildKeys.LOG)
                 .addTexture(ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.MINECRAFT, "oak_log"), PaletteStrategies.LOG_SIDE_STANDARD)
                 .setTab(() -> BuiltInRegistries.CREATIVE_MODE_TAB.get(CreativeModeTabs.BUILDING_BLOCKS))
                 .setTabMode(TabAddMode.AFTER_SAME_WOOD)
                 .addTag(ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.DIFFERENTIATE, "wood_stairs"), Registries.BLOCK, Registries.ITEM)
                 .addRecipe(ResourceLocation.fromNamespaceAndPath(Lepton.MOD_ID, "crafting/wood_slab/oak_wood_stairs"))
                 .build();
-        this.addEntry(woodStairs);
+//        this.addEntry(woodStairs);
 
         woodVerticalSlab = SimpleEntrySet.builder(WoodType.class, "wood_vertical_slab", LeptonBlocks.OAK.woods().getVerticalSlabBlock(),
                 () -> VanillaWoodTypes.OAK, s -> new VerticalSlabBlock(Utils.copyPropertySafe(s.log)))
+                .requiresChildren(VanillaWoodChildKeys.LOG)
                 .addTexture(ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.MINECRAFT, "oak_log"), PaletteStrategies.LOG_SIDE_STANDARD)
                 .setTab(() -> BuiltInRegistries.CREATIVE_MODE_TAB.get(CreativeModeTabs.BUILDING_BLOCKS))
                 .setTabMode(TabAddMode.AFTER_SAME_WOOD)
                 .addTag(ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.DIFFERENTIATE, "wood_vertical_slab"), Registries.BLOCK, Registries.ITEM)
                 .addRecipe(ResourceLocation.fromNamespaceAndPath(Lepton.MOD_ID, "crafting/wood_slab/oak_wood_vertical_slab"))
                 .build();
-        this.addEntry(woodVerticalSlab);
+//        this.addEntry(woodVerticalSlab);
 
         woodWall = SimpleEntrySet.builder(WoodType.class, "wood_wall", LeptonBlocks.OAK.woods().getWallBlock(),
                 () -> VanillaWoodTypes.OAK, s -> new WallBlock(Utils.copyPropertySafe(s.log)))
+                .requiresChildren(VanillaWoodChildKeys.WOOD)
                 .addTexture(ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.MINECRAFT, "oak_log"), PaletteStrategies.LOG_SIDE_STANDARD)
                 .setTab(() -> BuiltInRegistries.CREATIVE_MODE_TAB.get(CreativeModeTabs.BUILDING_BLOCKS))
                 .setTabMode(TabAddMode.AFTER_SAME_WOOD)
                 .addTag(ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.DIFFERENTIATE, "wood_wall"), Registries.BLOCK, Registries.ITEM)
                 .addRecipe(ResourceLocation.fromNamespaceAndPath(Lepton.MOD_ID, "crafting/wood_slab/oak_wood_wall"))
                 .build();
-        this.addEntry(woodWall);
+//        this.addEntry(woodWall);
+
+        strippedWoodSlab = SimpleEntrySet.builder(WoodType.class, "wood_slab", "stripped", LeptonBlocks.OAK.strippedWoods().getSlabBlock(),
+                        () -> VanillaWoodTypes.OAK, s -> new SlabBlock(Utils.copyPropertySafe(s.log)))
+                .requiresChildren(VanillaWoodChildKeys.STRIPPED_WOOD, VanillaWoodChildKeys.STRIPPED_LOG)
+                .addTexture(ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.MINECRAFT, "stripped_oak_log"), PaletteStrategies.LOG_SIDE_STANDARD)
+                .includeModelsBlock(
+                        ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.MINECRAFT, "stripped_oak_wood"),
+                        ResourceLocation.fromNamespaceAndPath(Lepton.MOD_ID, "stripped_oak_wood_slab"),
+                        ResourceLocation.fromNamespaceAndPath(Lepton.MOD_ID, "stripped_oak_wood_slab_top")
+                )
+                .setTab(() -> BuiltInRegistries.CREATIVE_MODE_TAB.get(CreativeModeTabs.BUILDING_BLOCKS))
+                .setTabMode(TabAddMode.AFTER_SAME_WOOD)
+                .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
+                .addTag(ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.DIFFERENTIATE, "wood_slabs"), Registries.BLOCK, Registries.ITEM)
+                .addRecipe(ResourceLocation.fromNamespaceAndPath(Lepton.MOD_ID, "crafting/wood_slab/stripped_oak_wood_slab"))
+                .build();
+        this.addEntry(strippedWoodSlab);
+
+        strippedWoodStairs = SimpleEntrySet.builder(WoodType.class, "stripped_wood_stairs", LeptonBlocks.OAK.strippedWoods().getStairsBlock(),
+                        () -> VanillaWoodTypes.OAK, s -> new StairBlock(VanillaWoodTypes.OAK.log.defaultBlockState(), Utils.copyPropertySafe(s.log)))
+                .addTexture(ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.MINECRAFT, "stripped_oak_log"), PaletteStrategies.LOG_SIDE_STANDARD)
+                .setTab(() -> BuiltInRegistries.CREATIVE_MODE_TAB.get(CreativeModeTabs.BUILDING_BLOCKS))
+                .setTabMode(TabAddMode.AFTER_SAME_WOOD)
+                .addTag(ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.DIFFERENTIATE, "wood_stairs"), Registries.BLOCK, Registries.ITEM)
+                .addRecipe(ResourceLocation.fromNamespaceAndPath(Lepton.MOD_ID, "crafting/wood_slab/stripped_oak_wood_stairs"))
+                .build();
+//        this.addEntry(strippedWoodStairs);
+
+        strippedWoodVerticalSlab = SimpleEntrySet.builder(WoodType.class, "stripped_wood_vertical_slab", LeptonBlocks.OAK.strippedWoods().getVerticalSlabBlock(),
+                        () -> VanillaWoodTypes.OAK, s -> new VerticalSlabBlock(Utils.copyPropertySafe(s.log)))
+                .addTexture(ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.MINECRAFT, "stripped_oak_log"), PaletteStrategies.LOG_SIDE_STANDARD)
+                .setTab(() -> BuiltInRegistries.CREATIVE_MODE_TAB.get(CreativeModeTabs.BUILDING_BLOCKS))
+                .setTabMode(TabAddMode.AFTER_SAME_WOOD)
+                .addTag(ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.DIFFERENTIATE, "wood_vertical_slab"), Registries.BLOCK, Registries.ITEM)
+                .addRecipe(ResourceLocation.fromNamespaceAndPath(Lepton.MOD_ID, "crafting/wood_slab/stripped_oak_wood_vertical_slab"))
+                .build();
+//        this.addEntry(strippedWoodVerticalSlab);
+
+        strippedWoodWall = SimpleEntrySet.builder(WoodType.class, "stripped_wood_wall", LeptonBlocks.OAK.strippedWoods().getWallBlock(),
+                        () -> VanillaWoodTypes.OAK, s -> new WallBlock(Utils.copyPropertySafe(s.log)))
+                .addTexture(ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.MINECRAFT, "stripped_oak_log"), PaletteStrategies.LOG_SIDE_STANDARD)
+                .setTab(() -> BuiltInRegistries.CREATIVE_MODE_TAB.get(CreativeModeTabs.BUILDING_BLOCKS))
+                .setTabMode(TabAddMode.AFTER_SAME_WOOD)
+                .addTag(ResourceLocation.fromNamespaceAndPath(CompatUtil.Mods.DIFFERENTIATE, "wood_wall"), Registries.BLOCK, Registries.ITEM)
+                .addRecipe(ResourceLocation.fromNamespaceAndPath(Lepton.MOD_ID, "crafting/wood_slab/stripped_oak_wood_wall"))
+                .build();
+//        this.addEntry(strippedWoodWall);
     }
 
     @Override
